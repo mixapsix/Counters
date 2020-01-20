79ca4f1f167c450dfcca29f7703ee5368c892555
mixapsix <mixapsix1@mail.ru>
2020-01-20 21:41:40 +0300

    Добавьте файлы проекта.

---
+++ Counters/Counter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Counters
+{
+    public class Counter
+    {
+        [Required]
+        public int ID { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ Counters/CountersContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Counters
+{
+    public class CountersContext : DbContext
+    {
+        public DbSet<Counter> Counters { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost; Port=5432; Database=postgres; Username=postgres; Password=Mixa4343");
+        }
+    }
+}
